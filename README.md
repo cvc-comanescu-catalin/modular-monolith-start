b9576d343748ded76010b8dc3890c9bc600570e2
Catalin Comanescu <cvc.comanescu.catalin@gmail.com>
2025-06-27 19:13:23 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # modular-monolith-start
+
 Getting started with modular monoliths
+
+https://github.com/cvc-comanescu-catalin/modular-monolith-start.git
+
+For this repo, go into to the relevant repo DIR and:
+git config user.name "Your Name Here"
+git config user.email your@email.example
+
+git remote set-url origin git@github.com:cvc-comanescu-catalin/modular-monolith-start.git
+git remote -v
+
+# SSH for remote authentication
+
+git config core.sshCommand 'ssh -o IdentitiesOnly=yes -i ~/.ssh/id_cvc_ed25519'
+
+# SSH for commit signing
+
+git config --global user.signingKey '~/.ssh/git_ed25519.pub'
+git config --global gpg.format ssh
+git config --global commit.gpgsign true
+
+mkdir src
+cd src
+dotnet new sln -n RiverBooks
+dotnet new webapi -n RiverBooks.Web
+dotnet sln RiverBooks.sln add .\RiverBooks.Web\RiverBooks.Web.csproj
+dotnet new classlib -n RiverBooks.Books -o RiverBooks.Books
+dotnet sln RiverBooks.sln add .\RiverBooks.Books\RiverBooks.Books.csproj
---
+++ src/RiverBooks.Books/Class1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RiverBooks.Books;
+
+internal interface IBookService
+{
+    IEnumerable<BookDto> ListBooks();
+}
+
+public record BookDto(Guid Id, string Title, string Author);
+
+internal class BookService : IBookService
+{
+    private readonly List<BookDto> _books =
+    [
+        new BookDto(Guid.NewGuid(), "The Great Gatsby", "F. Scott Fitzgerald"),
+        new BookDto(Guid.NewGuid(), "1984", "George Orwell"),
+        new BookDto(Guid.NewGuid(), "To Kill a Mockingbird", "Harper Lee"),
+    ];
+
+    public IEnumerable<BookDto> ListBooks()
+    {
+        return [.. _books];
+    }
+}
+
+public static class BookEndpoints
+{
+    public static void MapBookEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/books", async (IBookService bookService) =>
+        {
+            return bookService.ListBooks();
+        });
+    }
+}
+
+public static class BookServiceCollectionExtensions
+{
+    public static IServiceCollection AddBookServices(this IServiceCollection services)
+    {
+        services.AddScoped<IBookService, BookService>();
+        return services;
+    }
+}
